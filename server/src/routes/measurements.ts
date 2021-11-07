a78f45914073c47129afba77a3749046b55528ec
Jack Hermanson <jack.hermanson@live.com>
2021-11-06 19:44:58 -0600

    create and read measurements

--- server/src/routes/measurements.ts
+++ server/src/routes/measurements.ts
@@ -1,18 +1,26 @@
 import { Request, Response, Router } from "express";
 import { MeasurementRecord, MeasurementRequest } from "../../../shared";
+import { HTTP, validateRequest } from "jack-hermanson-ts-utils";
+import { measurementSchema } from "../models/Measurement";
+import { MeasurementService } from "../services/MeasurementService";
 
 export const router = Router();
 
-// router.post(
-//     "/",
-//     async (
-//         req: Request<MeasurementRequest>,
-//         res: Response<MeasurementRecord>
-//     ) => {
-//          await res.json(null);
-//     }
-// );
+router.post(
+    "/",
+    async (
+        req: Request<MeasurementRequest>,
+        res: Response<MeasurementRecord>
+    ) => {
+        if (!(await validateRequest(measurementSchema, req, res))) {
+            return;
+        }
+        const measurement = await MeasurementService.create(req.body);
+        res.status(HTTP.CREATED).json(measurement);
+    }
+);
 
-router.get("/", async (req: Request, res: Response) => {
-    res.json({});
+router.get("/", async (req: Request, res: Response<MeasurementRecord[]>) => {
+    const measurements = await MeasurementService.getAll();
+    res.json(measurements);
 });
