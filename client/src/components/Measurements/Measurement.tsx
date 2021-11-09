410a7141f838c7314dfe009d4f6ae7af02d3e615
Jack Hermanson <jack.hermanson@live.com>
2021-11-09 09:43:05 -0700

    format measurement output

--- client/src/components/Measurements/Measurement.tsx
+++ client/src/components/Measurements/Measurement.tsx
@@ -1,5 +1,5 @@
 import { MeasurementRecord } from "../../../../shared";
-import { FunctionComponent } from "react";
+import { Fragment, FunctionComponent } from "react";
 import { ActionCardHeader, KeyValCardBody } from "jack-hermanson-component-lib";
 import { Card } from "reactstrap";
 import Moment from "moment";
@@ -18,19 +18,30 @@ export const Measurement: FunctionComponent<Props> = ({
                 keyValPairs={[
                     {
                         key: "Temperature",
-                        val: measurement.temperature,
+                        val: (
+                            <Fragment>
+                                {measurement.temperature.toFixed(1)} &#8457;
+                            </Fragment>
+                        ),
                     },
                     {
                         key: "Humidity",
-                        val: measurement.humidity,
+                        val: (
+                            <Fragment>
+                                {(measurement.humidity * 100).toFixed(1)} %
+                            </Fragment>
+                        ),
                     },
                     {
                         key: "Generated",
                         val: Moment(measurement.generated).fromNow(),
                     },
                     {
-                        key: "Received",
-                        val: Moment(measurement.generated).fromNow(),
+                        key: "Received Delay",
+                        val: `${(
+                            Moment(measurement.created).toDate().getTime() -
+                            Moment(measurement.generated).toDate().getTime()
+                        ).toLocaleString()} milliseconds`,
                     },
                 ]}
             />
