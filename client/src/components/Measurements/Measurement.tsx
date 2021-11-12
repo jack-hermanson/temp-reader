cfca02e4ff7ed5ec91e0d64e8abce761ed0577b0
Jack Hermanson <jack.hermanson@live.com>
2021-11-12 08:54:43 -0700

    change card to table

--- client/src/components/Measurements/Measurement.tsx
+++ client/src/components/Measurements/Measurement.tsx
@@ -1,7 +1,6 @@
 import { MeasurementRecord } from "../../../../shared";
 import { Fragment, FunctionComponent } from "react";
-import { ActionCardHeader, KeyValCardBody } from "jack-hermanson-component-lib";
-import { Card } from "reactstrap";
+import { KeyValTable } from "jack-hermanson-component-lib";
 import Moment from "moment";
 import { millisecondToSecond } from "../../functions";
 
@@ -13,41 +12,17 @@ export const Measurement: FunctionComponent<Props> = ({
     measurement,
 }: Props) => {
     return (
-        <Card className="mb-3 no-mb-last">
-            <ActionCardHeader title={`Measurement #${measurement.id}`} />
-            <KeyValCardBody
-                keyValPairs={[
-                    {
-                        key: "Temperature",
-                        val: (
-                            <Fragment>
-                                {measurement.temperature.toFixed(1)} &#8457;
-                            </Fragment>
-                        ),
-                    },
-                    {
-                        key: "Humidity",
-                        val: (
-                            <Fragment>
-                                {(measurement.humidity * 100).toFixed(1)} %
-                            </Fragment>
-                        ),
-                    },
-                    {
-                        key: "Generated",
-                        val: Moment(measurement.generated)
-                            .local()
-                            .format("h:mm:ss a"),
-                    },
-                    {
-                        key: "Received Delay",
-                        val: `${millisecondToSecond(
-                            Moment(measurement.created).toDate().getTime() -
-                                Moment(measurement.generated).toDate().getTime()
-                        )} seconds`,
-                    },
-                ]}
-            />
-        </Card>
+        <tr>
+            <td>{measurement.temperature.toFixed(1)} &#8457;</td>
+            <td>{(measurement.humidity * 100).toFixed(1)} %</td>
+            <td>{Moment(measurement.generated).local().format("h:mm:ss a")}</td>
+            <td>
+                {millisecondToSecond(
+                    Moment(measurement.created).toDate().getTime() -
+                        Moment(measurement.generated).toDate().getTime()
+                )}{" "}
+                seconds
+            </td>
+        </tr>
     );
 };
--- client/src/components/Measurements/Measurements.tsx
+++ client/src/components/Measurements/Measurements.tsx
@@ -2,24 +2,33 @@ import { FunctionComponent } from "react";
 import { useStoreState } from "../../store";
 import { LoadingSpinner } from "jack-hermanson-component-lib";
 import { Measurement } from "./Measurement";
+import { Table } from "reactstrap";
 
 export const Measurements: FunctionComponent = () => {
     const measurements = useStoreState(state => state.measurements);
 
     return (
-        <div>
+        <Table striped>
+            <thead>
+                <tr>
+                    <th>Temp</th>
+                    <th>Humidity</th>
+                    <th>Generated</th>
+                    <th>Delay</th>
+                </tr>
+            </thead>
             {measurements ? (
-                <div>
+                <tbody>
                     {measurements.map(measurement => (
                         <Measurement
                             measurement={measurement}
                             key={measurement.id}
                         />
                     ))}
-                </div>
+                </tbody>
             ) : (
                 <LoadingSpinner />
             )}
-        </div>
+        </Table>
     );
 };
